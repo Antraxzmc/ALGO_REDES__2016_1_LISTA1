64b17e486a112260fad9b95cc7cef947f6111e51
Gabriel de Assis <antraxzmc@gmail.com>
2016-02-04 00:30:16 -0300

    Primeiro Commit

---
+++ atividadeGABRIEL.py
@@ -0,0 +1,22 @@
+print ("Hello World")
+a = "Eu tenho um sonho. O sonho de ver meus filhos julgados por sua personalidade, não pela cor de sua pele."
+print (a.upper())
+print (a.lower())
+print (a.title())
+b = 200
+c = 250
+d = 8
+e = 7
+f = 800
+g = 10
+h = 2
+i = b + c
+j = b / d
+l = b * e
+m = f - g
+n = h ** g
+print (i)
+print (j)
+print (l)
+print (m)
+print (n)
